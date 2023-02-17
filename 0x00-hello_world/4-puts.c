e3e28ea466243a4e4c59bea56bbac5db42af7967
paulinemaster2505 <122858170+paulinemaster2505@users.noreply.github.com>
2023-02-17 03:15:29 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+          puts("\"Programming is like building a multilingual puzzle");
+          return (0);
 }
 
