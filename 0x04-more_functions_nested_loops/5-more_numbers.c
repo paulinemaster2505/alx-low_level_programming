68384382410c9d13ce6d66d16e65e23f1dd95ceb
paulinemaster2505 <ppmasters17@gmail.com>
2023-02-23 06:09:03 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@
 
 void print_line(int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
